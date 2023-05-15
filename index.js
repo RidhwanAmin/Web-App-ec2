ce432ecf4916f03bcdda25680c5c78c670854e63
ridhwan <ridhwan@ridhwans-Air.AirPort>
2023-05-15 10:39:51 +0900

    new port

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 import express from 'express';
+import fs from 'fs';
+import https from 'https';
+
+const key = fs.readFileSync('private.key');
+const certificate = fs.readFileSync('certificate.crt');
 
 const app = express();
 
 app.listen(8080, () => console.log('API running on port 8080'))
 
-app.get('/', (req, res) => res.json('AWS'))
+app.get('/', (req, res) => {
+    res.send('AWS');
+  });
+  
